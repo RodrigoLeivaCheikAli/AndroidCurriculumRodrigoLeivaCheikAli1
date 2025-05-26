9f11cd3f3ccb1f92f5769328123627b9364af9cc
RodrigoLeivaCheikAli <141652879+RodrigoLeivaCheikAli@users.noreply.github.com>
2025-05-25 22:59:31 -0300

    Subiendo Trabajo de Curriculum

---
+++ Java/Ejercicio extra 1 Integrador A/src/ejercicio/extra/pkg1/integrador/a/EjercicioExtra1IntegradorA.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.extra.pkg1.integrador.a;
+
+/**
+ *
+ * @author User
+ */
+public class EjercicioExtra1IntegradorA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ExtrasGI.EquivalenciaMinutos();
+        //ExtrasGI.Valorescambiados();
+        //ExtrasGI.EsVocal();
+        //ExtrasGI.NrosRomanos();
+        //ExtrasGI.ObraSocial();
+        //ExtrasGI.AlturaDePersonas();
+        //ExtrasGI.MinyMax();
+        //ExtrasGI.ParyImpar();
+        //ExtrasGI.DivisionResta();
+        //ExtrasGI.NumeroMisterioso();
+        //ExtrasGI.Digitos();
+        //ExtrasGI.E_3version2();
+        ExtrasGI.E_3version1();
+        //ExtrasGI.MinyMaxversion2();
+        //ExtrasGI.EscalerasDeNumeros();
+    }
+    
+}
